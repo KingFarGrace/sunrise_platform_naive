23b859fae831fbe81d3ce24048decf4df968efaa
Silent_Wolf <976444057@qq.com>
2020-10-13 23:24:00 +0800

    feat: add login page scaffold

---
+++ front-end/sunrise-platform/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div id="login-card">
+      <form id="login-form">
+        <h2 id="login-title">登录</h2>
+        <label for="username">学号</label>
+        <input type="text" name="username" id="username" placeholder="学号"/>
+        <label for="password">密码</label>
+        <input type="password" name="password" id="password" placeholder="密码"/>
+        <input type="button" value="登录">
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
