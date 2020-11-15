f2000b8e5f7a6cc6e13b29cad12d8f5360818196
吴海源 <5703925+wu_hai_yuan@user.noreply.gitee.com>
2020-11-15 16:26:58 +0800

    add DruidConfig

--- back-end/managementplatform/src/main/java/team/sunrise/managementplatform/ManagementplatformApplication.java
+++ back-end/managementplatform/src/main/java/team/sunrise/managementplatform/ManagementplatformApplication.java
@@ -2,11 +2,14 @@ package team.sunrise.managementplatform;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author SilentWolf1997
  */
+
 @SpringBootApplication
 @MapperScan("team.sunrise.managementplatform.mapper")
 public class ManagementplatformApplication {
